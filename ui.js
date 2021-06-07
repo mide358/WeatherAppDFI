afc249bf94d01c2edc37c61db945b7a3967e178f
mide358 <iyimide.adegunloye@gmail.com>
2021-06-07 19:50:18 +0100

    addition of date and scroll

--- ui.js
+++ ui.js
@@ -1,4 +1,4 @@
-// SELECT NECESSARY ELEMENTS
+// class constructor to select ui elements
 class UI {
   constructor() {
     this.location = document.querySelector("#location");
@@ -12,17 +12,21 @@ class UI {
     this.wind = document.querySelector("#wind");
     this.latlon = document.querySelector("#latlon");
     this.locRight = document.querySelector("#location-right");
+    this.date = document.querySelector("#date");
   }
 
+  //  Adding elements to the UI with paint
+
   paint(weather) {
-    this.location.textContent = `${weather.location.name}`;
+    this.location.textContent = `${weather.location.name}, ${weather.location.country}`;
     this.temp.textContent = `${weather.current.temp_c}°c(${weather.current.temp_f}°f)`;
+    this.date.textContent = `Date: ${weather.location.localtime.slice(0, 10)}`;
 
     this.desc.textContent = weather.current.condition.text;
     this.icon.setAttribute("src", weather.current.condition.icon);
     this.locRight.textContent = `Location: ${weather.location.name}, ${weather.location.country}`;
     this.humidity.textContent = `Humidity: ${weather.current.humidity}%`;
-    this.time.textContent = `Time: ${weather.location.localtime}`;
+    this.time.textContent = `Time: ${weather.location.localtime.slice(10)}`;
     this.feelsLike.textContent = `Feels like: ${weather.current.feelslike_c}°C (${weather.current.feelslike_f}°F)`;
     this.wind.textContent = `Wind: ${weather.current.wind_mph}`;
     this.latlon.textContent = `Lat: ${weather.location.lat} & Lon: ${weather.location.lon} `;
